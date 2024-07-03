ce559f8e36aa9bbff9dd83cf9040ed0acb3971ac
miller <millerma@lap-miller.local>
2024-07-03 13:01:21 -0400

    stuff for getting bt, bp

--- data_access.py
+++ data_access.py
@@ -5586,7 +5586,7 @@ class Equilibrium(object):
                 s=0
             )
             return self._psiOfRZSpline[idx]
-            
+
     def rz2BZ(self, R, Z, t, return_t=False, make_grid=False, each_t=True, length_unit=1):
         r"""Calculates the vertical component of the magnetic field at the given (R, Z, t) coordinates.
         
@@ -5915,6 +5915,113 @@ class Equilibrium(object):
             return unit_factor * B_T, blob
         else:
             return unit_factor * B_T
+
+    def rz2F(self, R, Z, t, **kwargs):
+        r"""Calculates the flux function :math:`F=RB_{\phi}` at the given (R, Z, t).
+        
+        By default, EFIT only computes this inside the LCFS.
+        
+        Args:
+            R (Array-like or scalar float): Values of the radial coordinate to
+                map to F. If `R` and `Z` are both scalar values,
+                they are used as the coordinate pair for all of the values in
+                `t`. Must have the same shape as `Z` unless the `make_grid`
+                keyword is set. If the `make_grid` keyword is True, `R` must
+                have exactly one dimension.
+            Z (Array-like or scalar float): Values of the vertical coordinate to
+                map to F. If `R` and `Z` are both scalar values,
+                they are used as the coordinate pair for all of the values in
+                `t`. Must have the same shape as `R` unless the `make_grid`
+                keyword is set. If the `make_grid` keyword is True, `Z` must
+                have exactly one dimension.
+            t (Array-like or scalar float): Times to perform the conversion at.
+                If `t` is a single value, it is used for all of the elements of
+                `R`, `Z`. If the `each_t` keyword is True, then `t` must be
+                scalar or have exactly one dimension. If the `each_t` keyword is
+                False, `t` must have the same shape as `R` and `Z` (or their
+                meshgrid if `make_grid` is True).
+        
+        Keyword Args:
+            sqrt (Boolean): Set to True to return the square root of F. 
+                Only the square root of positive values is taken. Negative 
+                values are replaced with zeros, consistent with Steve Wolfe's
+                IDL implementation efit_rz2rho.pro. Default is False.
+            each_t (Boolean): When True, the elements in `R`, `Z` are evaluated 
+                at each value in `t`. If True, `t` must have only one dimension
+                (or be a scalar). If False, `t` must match the shape of `R` and
+                `Z` or be a scalar. Default is True (evaluate ALL `R`, `Z` at
+                EACH element in `t`).
+            make_grid (Boolean): Set to True to pass `R` and `Z` through
+                :py:func:`scipy.meshgrid` before evaluating. If this is set to
+                True, `R` and `Z` must each only have a single dimension, but
+                can have different lengths. Default is False (do not form
+                meshgrid).
+            length_unit (String or 1): Length unit that `R`, `Z` are given in.
+                If a string is given, it must be a valid unit specifier:
+                
+                    ===========  ===========
+                    'm'          meters
+                    'cm'         centimeters
+                    'mm'         millimeters
+                    'in'         inches
+                    'ft'         feet
+                    'yd'         yards
+                    'smoot'      smoots
+                    'cubit'      cubits
+                    'hand'       hands
+                    'default'    meters
+                    ===========  ===========
+                
+                If length_unit is 1 or None, meters are assumed. The default
+                value is 1 (use meters).
+            return_t (Boolean): Set to True to return a tuple of (`F`,
+                `time_idxs`), where `time_idxs` is the array of time indices
+                actually used in evaluating `F` with nearest-neighbor
+                interpolation. (This is mostly present as an internal helper.)
+                Default is False (only return `F`).
+        
+        Returns:
+            `F` or (`F`, `time_idxs`)
+            
+            * **F** (`Array or scalar float`) - The flux function :math:`F=RB_{\phi}`.
+              If all of the input arguments are scalar, then a scalar is
+              returned. Otherwise, a scipy Array is returned. If `R` and `Z`
+              both have the same shape then `F` has this shape as well,
+              unless the `make_grid` keyword was True, in which case `F`
+              has shape (len(`Z`), len(`R`)).
+            * **time_idxs** (Array with same shape as `F`) - The indices 
+              (in :py:meth:`self.getTimeBase`) that were used for
+              nearest-neighbor interpolation. Only returned if `return_t` is
+              True.
+       
+        Examples:
+            All assume that `Eq_instance` is a valid instance of the
+            appropriate extension of the :py:class:`Equilibrium` abstract class.
+            
+            Find single F value at R=0.6m, Z=0.0m, t=0.26s::
+            
+                F_val = Eq_instance.rz2F(0.6, 0, 0.26)
+            
+            Find F values at (R, Z) points (0.6m, 0m) and (0.8m, 0m) at the
+            single time t=0.26s. Note that the `Z` vector must be fully specified,
+            even if the values are all the same::
+            
+                F_arr = Eq_instance.rz2F([0.6, 0.8], [0, 0], 0.26)
+            
+            Find F values at (R, Z) points (0.6m, 0m) at times t=[0.2s, 0.3s]::
+            
+                F_arr = Eq_instance.rz2F(0.6, 0, [0.2, 0.3])
+            
+            Find F values at (R, Z, t) points (0.6m, 0m, 0.2s) and (0.5m, 0.2m, 0.3s)::
+            
+                F_arr = Eq_instance.rz2F([0.6, 0.5], [0, 0.2], [0.2, 0.3], each_t=False)
+            
+            Find F values on grid defined by 1D vector of radial positions `R`
+            and 1D vector of vertical positions `Z` at time t=0.2s::
+            
+                F_mat = Eq_instance.rz2F(R, Z, 0.2, make_grid=True)
+        """
+        return self._RZ2Quan(self._getFSpline, R, Z, t, **kwargs)
 
 
 class EFITTree(Equilibrium):
